1faf67beeb1b88661d193270fa5c0a43948230a7
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-10-11 16:06:54 +0200

    Fix provider notification Also clean code to somethings mere elegant

--- providers/default.rb
+++ providers/default.rb
@@ -1,59 +1,44 @@
 use_inline_resources
 
 action :sync do
-  ::File.delete(hgup_file) if ::File.exist?(hgup_file)
-  execute "sync repository #{new_resource.path}" do
+  clone
+  sync
+end
+
+action :clone do
+  clone
+end
+
+def clone
+  execute "clone repository #{new_resource.path}" do
+    command "hg clone --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository} #{new_resource.path}"
+    user new_resource.owner
+    group new_resource.group
     not_if "hg identify #{new_resource.path}"
-    command "hg clone --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository} #{new_resource.path} && touch #{hgup_file}"
-    creates hgup_file
-    notifies :run, "execute[set ownership #{new_resource.path}]"
-    notifies :run, "execute[set permissions #{new_resource.path}]"
   end
-  execute "check incoming changes #{new_resource.path}" do
-    command "hg incoming --rev #{new_resource.reference} #{hg_connection_command}  #{new_resource.repository} && touch #{hgup_file} || true"
-    cwd new_resource.path
-    creates hgup_file
-    notifies :run, "execute[pull #{new_resource.path}]"
-  end
+end
+
+def sync
   execute "pull #{new_resource.path}" do
-    command "hg pull --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository}"
+    command "hg unbundle #{bundle_file}"
+    user new_resource.owner
+    group new_resource.group
     cwd new_resource.path
-    only_if { ::File.exist?(hgup_file) }
-    action :nothing
+    only_if "hg incoming --rev #{new_resource.reference} #{hg_connection_command} --bundle #{bundle_file} #{new_resource.repository}",
+       :cwd => new_resource.path, 
+       :user => new_resource.owner, 
+       :group => new_resource.group
     notifies :run, "execute[update #{new_resource.path}]"
   end
   execute "update #{new_resource.path}" do
     command "hg update"
+    user new_resource.owner
+    group new_resource.group
     cwd new_resource.path
     action :nothing
-    notifies :run, "execute[set ownership #{new_resource.path}]"
-    notifies :run, "execute[set permissions #{new_resource.path}]"
-  end
-  execute "set ownership #{new_resource.path}" do
-    command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
-    action :nothing
-  end
-  execute "set permissions #{new_resource.path}" do
-    command "chmod -R #{new_resource.mode} #{new_resource.path}"
-    only_if { new_resource.mode }
   end
 end
 
-action :clone do
-  ::File.delete(hgup_file) if ::File.exist?(hgup_file)
-  execute "clone repository #{new_resource.path}" do
-    command "hg clone --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository} #{new_resource.path}"
-    not_if "hg identify #{new_resource.path}"
-    notifies :run, "execute[set permission #{new_resource.path}]"
-    notifies :run, "execute[set ownership #{new_resource.path}]"
-  end
-  execute "set ownership #{new_resource.path}" do
-    command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
-    action :nothing
-  end
-  execute "set permission #{new_resource.path}" do
-    command "chmod -R #{new_resource.mode} #{new_resource.path}"
-    action :nothing
-    only_if { new_resource.mode }
-  end
+def bundle_file
+  return ::File.join(Chef::Config[:file_cache_path], "mercurial.bundle")
 end
